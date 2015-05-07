951612a92f1695b8c130ba9bb65ec4ac1b66c2b6
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2015-05-07 22:16:54 +0200

    ## Container
    With styles in place, let's get our hands on extracting a shared layout for all future views to come.
    Start by adding `container` parameter to `index` in `View`:
    
    ==> View.fs:26-31
    
    and div with id "container" just after the div "header":
    
    ==> View.fs:33-38
    
    `index` previously was a constant value, but it has now become a function taking `container` as parameter.
    
    We can now define the actual container for the "home" page:
    
    ==> View.fs:10-12
    
    For now it will only contain plain "Home" text.
    Let's also extract a common function for creating the WebPart, parametrized with the container itself.
    Add to `App` module, just before the `browse` WebPart the following:
    
    ==> App.fs:9-10
    
    Usage for the home page looks like this:
    
    ==> App.fs:19-20
    
    Next, containers for each valid route in our application can be defined in `View` module:
    
    ==> View.fs:10-24
    
    Note that both `home` and `store` are constant values, while `browse` and `details` are parametrized with `genre` and `id` respectively.
    
    `html` can be now reused for all 4 views:
    
    ==> App.fs:12-26

--- View.fs
+++ View.fs
@@ -7,7 +7,23 @@ let h1 xml = tag "h1" [] xml
 let aHref href = tag "a" ["href", href]
 let cssLink href = linkAttr [ "href", href; " rel", "stylesheet"; " type", "text/css" ]
 
-let index = 
+let home = [
+    text "Home"
+]
+
+let store = [
+    text "Store"
+]
+
+let browse genre = [
+    text (sprintf "Genre: %s" genre)
+]
+
+let details id = [
+    text (sprintf "Details %d" id)
+]
+
+let index container = 
     html [
         head [
             title "Suave Music Store"
@@ -18,6 +34,8 @@ let index =
             divId "header" [
                 h1 (aHref Path.home (text "F# Suave Music Store"))
             ]
+
+            divId "container" container
 
             divId "footer" [
                 text "built with "
